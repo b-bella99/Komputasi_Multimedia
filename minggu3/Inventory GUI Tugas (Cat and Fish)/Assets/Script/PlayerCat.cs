d58d6d8014cedc9f8451070618f817898190147e
b-bella99 <bella.setyowati99@gmail.com>
2021-02-24 15:30:16 +0700

    Upload hasil tugas dan laporan jobsheet Inventory GUI

---
+++ minggu3/Inventory GUI Tugas (Cat and Fish)/Assets/Script/PlayerCat.cs
@@ -0,0 +1,32 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class PlayerCat : MonoBehaviour
+{
+    public Text fishText;
+    public PlayerInventoryDisplay playerInventoryDisplay;
+    private int totalfishs = 0;
+    void Start()
+    {
+        playerInventoryDisplay = GetComponent<PlayerInventoryDisplay>();
+        UpdateFishText();
+    }
+    void OnTriggerEnter2D(Collider2D hit)
+    {
+        if (hit.CompareTag("Fish"))
+        {
+            totalfishs++;
+            UpdateFishText();
+            playerInventoryDisplay.OnChangeFishTotal(totalfishs);
+            Destroy(hit.gameObject);
+        }
+    }
+    private void UpdateFishText()
+    {
+        string fishMessage = "Fish = " + totalfishs;
+        fishText.text = fishMessage;
+    }
+
+}
---
+++ minggu3/Inventory GUI Tugas (Cat and Fish)/Assets/Script/PlayerInventoryDisplay.cs
@@ -0,0 +1,22 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class PlayerInventoryDisplay : MonoBehaviour
+{
+    public Image[] fishPlaceholders;
+    public Sprite iconFishRed;
+    public Sprite iconBoneFish;
+    public void OnChangeFishTotal(int fishTotal)
+    {
+        for (int i = 0; i < fishPlaceholders.Length; ++i)
+        {
+            if (i < fishTotal)
+                fishPlaceholders[i].sprite = iconFishRed;
+            else
+                fishPlaceholders[i].sprite = iconBoneFish;
+        }
+    }
+
+}
